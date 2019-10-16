466e805f4b9ebfa99fb511effa436ac565d286e8
semyonmoroshek <s.moroseks@gmail.com>
2019-10-16 15:17:22 +0300

    Basic methods

--- app/src/main/java/com/example/android/healthtest/MainActivity.java
+++ app/src/main/java/com/example/android/healthtest/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.android.healthtest.entities.Question;
+
 public class MainActivity extends AppCompatActivity {
 
     int healthLevel = 0;
@@ -19,73 +21,92 @@ public class MainActivity extends AppCompatActivity {
         displayCheckHealth("Check health");
     }
 
+    public void startPush(View view) {
+        Button startButton = (Button) findViewById(R.id.startButton);
+        startButton.setVisibility(View.INVISIBLE);
+        buttonClicked++;
+        displayNewQuestion();
+    }
+
+    public void displayNewQuestion() {
+        Question question = new Question();
+        TextView textDisplayQuestion = (TextView) findViewById(R.id.displayMessage);
+        if (buttonClicked == 1) {
+            textDisplayQuestion.setText(question.question1);
+        }
+        if (buttonClicked == 2) {
+            textDisplayQuestion.setText(question.question2);
+        }
+        if (buttonClicked == 3) {
+            textDisplayQuestion.setText(question.question3);
+        }
+        if (buttonClicked > 3) {
+            Button startButton = (Button) findViewById(R.id.startButton);
+            startButton.setVisibility(View.VISIBLE);
+            displayResult();
+            healthLevel = 0;
+            buttonClicked = 0;
+        }
+    }
+
+    public void displayResult() {
+        if (healthLevel <= 1) {
+            displayQuestion("You have a bad health");
+        }
+        if (healthLevel == 2) {
+            displayQuestion("You have a normal health");
+        }
+        if (healthLevel == 3) {
+            displayQuestion("You have a good health");
+        }
+    }
+
     public void displayTitle(String text) {
         TextView t = (TextView) findViewById(R.id.displayTitle);
         t.setText(text);
     }
 
     public void displayQuestion(String text) {
-        TextView t = (TextView) findViewById(R.id.displayQuestions);
+        TextView t = (TextView) findViewById(R.id.displayMessage);
         t.setText(text);
     }
-
-//    public void displayHealthPoint(int text) {
-//        TextView t = (TextView) findViewById(R.id.displayMessage);
-//        t.setText(text + "");
-//    }
 
     public void displayCheckHealth(String text) {
         displayTitle(text);
     }
 
     public void yes(View view) {
-        buttonClicked++;
-        healthLevel++;
-        pushButton();
+        if (buttonClicked > 0 && buttonClicked <= 3) {
+            buttonClicked++;
+            healthLevel++;
+            displayNewQuestion();
+        }
+        if(buttonClicked > 3){
+            displayResult();
+        }
     }
 
     public void sometimes(View view) {
-        buttonClicked++;
-        pushButton();
+        if (buttonClicked > 0 && buttonClicked <= 3) {
+            buttonClicked++;
+            displayNewQuestion();
+        }
+        if(buttonClicked > 3){
+            displayResult();
+        }
     }
 
     public void no(View view) {
-        buttonClicked++;
-        healthLevel--;
-        pushButton();
-    }
-
-    public void pushButton(){
-        if(buttonClicked == 1){
-            displayQuestion("Are you eat healthy food? ");
+        if (buttonClicked > 0 && buttonClicked <= 3) {
+            buttonClicked++;
+            if(healthLevel > 0) {
+                healthLevel--;
+            }
+            displayNewQuestion();
         }
-        if(buttonClicked == 2){
-            displayQuestion("Are you drink steal water more then 2 l/day ");
-        }
-        if(buttonClicked > 2){
+        if(buttonClicked > 3){
             displayResult();
         }
-
-    }
-
-    public void displayResult() {
-        if(healthLevel < 2){
-            displayQuestion("You have a bad health");
-        }
-        if(healthLevel == 2){
-            displayQuestion("You have a normal health");
-        }
-        if(healthLevel >2){
-            displayQuestion("You have a good health");
-        }
-    }
-
-
-    public void start(View view) {
-        Button t = (Button) findViewById(R.id.startButton);
-        t.setVisibility(View.INVISIBLE);
-        displayQuestion("Are you sleep more then 7 our/night?");
-
     }
 }
 
---
+++ app/src/main/java/com/example/android/healthtest/entities/Question.java
@@ -0,0 +1,10 @@
+package com.example.android.healthtest.entities;
+
+public class Question {
+
+    public String question1 = "Are you eat healthy food? ";
+    public String question2 = "Are you drink steal water more then 2 L/day? ";
+    public String question3 = "Are you sleep more then 7 our/night?";
+
+
+}
