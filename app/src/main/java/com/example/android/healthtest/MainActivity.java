edfb855b1d628a20587c3a4b777f8c40dfa950be
semyonmoroshek <s.moroseks@gmail.com>
2020-02-28 20:03:26 +0200

    Refactoring

--- app/src/main/java/com/example/android/healthtest/MainActivity.java
+++ app/src/main/java/com/example/android/healthtest/MainActivity.java
@@ -3,110 +3,87 @@ package com.example.android.healthtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.example.android.healthtest.entities.Question;
+import com.example.android.healthtest.model.Question;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
     int healthLevel = 0;
     int buttonClicked = 0;
 
+    @BindView(R.id.btn_yes) Button btnYes;
+    @BindView(R.id.btn_sometms) Button btnSometimes;
+    @BindView(R.id.btn_no) Button btnNo;
+    @BindView(R.id.txt_question) TextView txtQuestion;
+    @BindView(R.id.pb_progress) ProgressBar userProgressBar;
+
+    private int mQuestionIndex = 0;
+    private int mQuestion;
+    private int yesAnswerCount = 0;
+    private int noAnswerCount = 0;
+    private int sometimesAnswerCount = 0;
+    private int totalScore = 0;
+
+    private Question[] questionCollection = new Question[]{
+            new Question(R.string.q1),
+            new Question(R.string.q2),
+            new Question(R.string.q3),
+            new Question(R.string.q4),
+            new Question(R.string.q5)};
+
+    int userProgresStep = (int) Math.ceil(100.0 / questionCollection.length);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayCheckHealth("Check health");
+        ButterKnife.bind(this);
+
+        displayNewQuestion();
+
+        btnYes.setOnClickListener(View -> yesAnswer());
+        btnSometimes.setOnClickListener(View -> sometimesAnswer());
+        btnNo.setOnClickListener(View -> noAnswer());
     }
 
-    public void startPush(View view) {
-        Button startButton = (Button) findViewById(R.id.startButton);
-        startButton.setVisibility(View.INVISIBLE);
-        buttonClicked++;
+    private void sometimesAnswer() {
+        sometimesAnswerCount++;
         displayNewQuestion();
     }
 
-    public void displayNewQuestion() {
-        Question question = new Question();
-        TextView textDisplayQuestion = (TextView) findViewById(R.id.displayMessage);
-        if (buttonClicked == 1) {
-            textDisplayQuestion.setText(question.question1);
-        }
-        if (buttonClicked == 2) {
-            textDisplayQuestion.setText(question.question2);
-        }
-        if (buttonClicked == 3) {
-            textDisplayQuestion.setText(question.question3);
-        }
-        if (buttonClicked > 3) {
-            Button startButton = (Button) findViewById(R.id.startButton);
-            startButton.setVisibility(View.VISIBLE);
-            displayResult();
-            healthLevel = 0;
-            buttonClicked = 0;
+    private void noAnswer() {
+        noAnswerCount++;
+        displayNewQuestion();
+    }
+
+    private void yesAnswer() {
+        yesAnswerCount++;
+        displayNewQuestion();
+    }
+
+    private void displayNewQuestion() {
+        if (mQuestionIndex < questionCollection.length) {
+            Question question = questionCollection[mQuestionIndex];
+            mQuestion = question.getQuestionNumber();
+            txtQuestion.setText(mQuestion);
+            userProgressBar.incrementProgressBy(userProgresStep);
+            mQuestionIndex++;
+        } else {
+            resetApp();
         }
     }
 
-    public void displayResult() {
-        if (healthLevel <= 1) {
-            displayQuestion("You have a bad health");
-        }
-        if (healthLevel == 2) {
-            displayQuestion("You have a normal health");
-        }
-        if (healthLevel == 3) {
-            displayQuestion("You have a good health");
-        }
-    }
-
-    public void displayTitle(String text) {
-        TextView t = (TextView) findViewById(R.id.displayTitle);
-        t.setText(text);
-    }
-
-    public void displayQuestion(String text) {
-        TextView t = (TextView) findViewById(R.id.displayMessage);
-        t.setText(text);
-    }
-
-    public void displayCheckHealth(String text) {
-        displayTitle(text);
-    }
-
-    public void yes(View view) {
-        if (buttonClicked > 0 && buttonClicked <= 3) {
-            buttonClicked++;
-            healthLevel++;
-            displayNewQuestion();
-        }
-        if(buttonClicked > 3){
-            displayResult();
-        }
-    }
-
-    public void sometimes(View view) {
-        if (buttonClicked > 0 && buttonClicked <= 3) {
-            buttonClicked++;
-            displayNewQuestion();
-        }
-        if(buttonClicked > 3){
-            displayResult();
-        }
-    }
-
-    public void no(View view) {
-        if (buttonClicked > 0 && buttonClicked <= 3) {
-            buttonClicked++;
-            if(healthLevel > 0) {
-                healthLevel--;
-            }
-            displayNewQuestion();
-        }
-        if(buttonClicked > 3){
-            displayResult();
-        }
+    private void resetApp() {
+        Toast.makeText(this, "your result: " + totalScore, Toast.LENGTH_LONG).show();
+        mQuestionIndex = 0;
+        userProgressBar.setProgress(0);
+        displayNewQuestion();
     }
 }
-
---
+++ app/src/main/java/com/example/android/healthtest/model/Question.java
@@ -0,0 +1,18 @@
+package com.example.android.healthtest.model;
+
+public class Question {
+
+    private int mQuestionNumber;
+
+    public Question(int questionNumber) {
+        mQuestionNumber = questionNumber;
+    }
+
+    public int getQuestionNumber() {
+        return mQuestionNumber;
+    }
+
+    public void setQuestionNumber(int questionNumber) {
+        mQuestionNumber = questionNumber;
+    }
+}
